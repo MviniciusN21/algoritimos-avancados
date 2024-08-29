829872540b7cbd79d255c7f404ecb6ededc9a46d
Marcos Vinicius Nascimento <Viniciusbuffon01@gmail.com>
2024-08-29 18:50:31 -0300

    Update and rename ex.2 to exec-2.py

--- exec-2.py
+++ exec-2.py
@@ -1,22 +1,15 @@
-#EX6
-print(" Os pares de instruções abaixo produzem o mesmo resultado?")
+#  Os pares de instruções abaixo produzem o mesmo resultado?
 
 print("LETRA A")
-print("4/2+2/4")
 print(4/2+2/4)
-print("(4/2)+(2/4)")
 print((4/2)+(2/4))
 
 print("LETRA B")
-print("4/(2+2)/4")
 print(4/(2+2)/4)
-print("4/2+2/4")
 print(4/2+2/4)
 
 print("LETRA C")
-print("(4+2)*2-4")
 print ((4+2)*2-4)
-print("4+2*2-4")
 print(4+2*2-4)
 
 print("Não são iguais!")
