0c35319fb88f882633e008866d8ec3e5c2f47096
Marcos Vinicius Nascimento <Viniciusbuffon01@gmail.com>
2024-08-28 21:40:03 -0300

    Update and rename ex1.txt to exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,17 @@
+# Ex 1
+
+#"1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis."
+
+A=10
+B=20
+
+print(" o valor de A é: a",A)
+print("o valor de B é: ",B)
+
+TEMP = A
+
+A=B
+B=TEMP
+
+print(" o valor de A é: a",A)
+print("o valor de B é: ",B)
